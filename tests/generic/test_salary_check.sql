dbef1da9b9f5473b66e31bc7b0ea796fbb75c882
Easter Biswas <easter.biswas@gmail.com>
2026-06-23 00:02:37 +0000

    commit custom generic test

--- tests/generic/test_salary_check.sql
+++ tests/generic/test_salary_check.sql
@@ -1,7 +1,7 @@
-{% tests salary_check(model,column_name) %}
+{% test salary_check(model, column_name) %}
 
-select * from 
-{{model}}
-where {{column_name}} < 40000
+select *
+from {{ model }}
+where {{ column_name }} < 40000
 
-{% endtests %}
+{% endtest %}
